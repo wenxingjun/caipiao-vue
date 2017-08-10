5d850497e7930737818cc680b933575e71b8e879
wenxingjun <254317842@qq.com>
2017-08-10 16:35:06 +0800

    添加消息详情页组件

--- src/views/users/message-details.vue
+++ src/views/users/message-details.vue
@@ -1,39 +1,71 @@
 <template>
-	<div data-page="message-details">
-		<div class="page-content">
-			<div class="contents">
-				小时候，总以为感情会是一场执子之手，白头偕老的约定。长大了，才知道每一段旅行的同路人，都只能走一小段，也只有多年之后回头，相视一笑，点点头
-			</div>
-			<div class="ending">
-				2017-02-23 16:20
+	<div class="list right-list" data-page="user-info-list">
+		<div class="mui-table-view table-view none-list">
+			<div  class="list-show" >
+				<div class="title" v-for="t in ListVal" v-if="t._id===num">
+					<div class="title">
+						<h4>{{t.describe}}</h4>
+						<div class="created">{{t.created}}</div>
+					</div>
+					<div class="content">{{t.content}}</div>
+				</div>
 			</div>
 		</div>
 	</div>
 </template>
 <script>
-export default {
-	data() {
-		return {};
-	},
-	mounted(){
-		this.$store.dispatch("updateNavbarTitle", "消息标题");
-	},
-	methods: {
-		
+	export default {
+		props: {
+			ListVal: {
+				type: Array,
+				required: true
+			},
+			num:{
+				type: Number,
+				required: true
+			}
+		},
+		components: {
+
+		},
+		created: function() {
+
+		},
+		methods: {
+
+		},
+		data() {
+			return {
+
+			}
+		}
 	}
-}
 </script>
 <style lang="less" scoped>
-	[data-page='message-details'] {
-		.contents {
-			padding: 20px;
-		}
-		
-		.ending {
-			font-size: 10px;
-			color: gray;
-		    text-align: right;
-		    padding: 20px;
-		}
+	[data-page='user-info-list'] {
+	.list-show {
+	.title {
+		margin-bottom: 1rem;
+		font-size: 1rem;
+		padding: 5px;
+		text-align: center;
+		border-bottom: 1px solid #e0e6ef;
+	h4{
+		margin-top: -1rem;
+	}
+	.created{
+		font-size: 0.5rem;
+	}
+	}
+	.content{
+		margin-top: 1rem;
+	}
+	}
+	.list-info{
+	.li{
+		display: flex;
+	}
+	}
 	}
 </style>
+
