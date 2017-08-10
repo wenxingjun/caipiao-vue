8054e12785ee3a03ece6f9dab7d3a1549a5bf2e8
wenxingjun <254317842@qq.com>
2017-08-10 16:36:20 +0800

    添加了消息列表组件

--- src/views/users/my-message-list.vue
+++ src/views/users/my-message-list.vue
@@ -1,56 +1,98 @@
 <template>
-	<div data-page="my-message-list">
-		<div class="page-content">
-			<ul class="mui-table-view mui-table-view-chevron">
-				<li class="mui-table-view-cell mui-media" @tap.stop.prevent="gotoDetails">
-					<a class="mui-navigate-right">
-						<div class="mui-media-object mui-pull-left item-date">02.14</div>
-						<div class="mui-media-body">
-							消息一
-							<p class="mui-ellipsis">烤炉模式的城，到黄昏，如同打翻的调色盘一般.</p>
-						</div>
-						<span class="mui-badge mui-badge-danger">新</span>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a class="mui-navigate-right" href="javascript:;">
-						<div class="mui-media-object mui-pull-left item-date">01.14</div>
-						<div class="mui-media-body">
-							消息二
-							<p class="mui-ellipsis">静静的看这个世界，最后终于疯了</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a class="mui-navigate-right">
-						<div class="mui-media-object mui-pull-left item-date">01.04</div>
-						<div class="mui-media-body">
-							消息三
-							<p class="mui-ellipsis">能和心爱的人一起睡觉，是件幸福的事情；可是，打呼噜怎么办？</p>
-						</div>
-					</a>
-				</li>
-			</ul>
+	<div class="mui-content account" id="tabs">
+		<div class="top">
+			<div class="mui-row">
+				<div class="mui-col-sm-5" style="border-right: 1px solid #e0e6ef;">
+					<a class="mui-action-back mui-icon mui-icon-left-nav mui-pull-left"></a>
+					<div class="tc">我的消息</div>
+				</div>
+				<div class="mui-col-sm-7">
+					<div class="tc" id="title" v-for="i in titleList">{{i}}</div>
+				</div>
+			</div>
+		</div>
+		<div class="account-content">
+			<div class="mui-row" id="account-list">
+				<div class="mui-col-sm-5">
+					<div class="left">
+						<ul class="mui-table-view mui-table-view-striped mui-table-view-condensed">
+							<li class="mui-table-view-cell" @tap.stop.prevent="toggle(index,tab._id)" v-for="(tab,index) in tabList" :class="{active:active==index}" v-model="tabNum" :tab-num="tabNum">
+								<div class="mui-table">
+									<div class="mui-table-cell mui-col-xs-10">
+										<h6 class="mui-ellipsis left-title">{{tab.type}}</h6>
+										<p class="mui-h6 mui-ellipsis left-describe">{{tab.describe}}</p>
+									</div>
+									<div class="mui-table-cell mui-col-xs-2 mui-text-right left-created">
+										<span class="mui-h5">{{tab.created}}</span>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="mui-col-sm-7">
+					<div class="right">
+						<tab :ListVal="tabList" :num="tabNum"></tab>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
+
 <script>
-export default {
-	data() {
-		return {};
-	},
-	methods: {
-		gotoDetails(){
-			this.$router.push({name: "messageDetails"});
+	import tab from './message-details.vue'
+	export default {
+		components: {
+			tab
+		},
+		created: function () {
+			var _this = this;
+			app.api.userInfo.userInfoList({
+				data: {
+					created_min:this.starTime,
+					created_max:this.endTime,
+				},
+				success(response){
+					_this.tabList = response.data;
+					_this.titleList=['消息详情'];
+					console.info(_this.tabList);
+				},
+				complete(){
+				}
+			});
+		},
+		data(){
+			return {
+				starTime: null,
+				endTime: null,
+				tabType:'消息详情',
+				tabNum:1,
+				tabList:[],
+				pages:{},
+			}
+		},
+		methods: {
+			toggle(i,j) {
+				this.active = i;
+				this.tabNum=j;
+			},
+		},
+		mounted () {
 		}
 	}
-}
 </script>
-<style lang="less" scoped>
-	[data-page='my-message-list'] {
-		.item-date {
-			font-size: 10px;
-			color: gray;
-		}
+<style scoped>
+	#tabs{
+		background: #eff5ff !important;
+	}
+	.left-title{
+
+	}
+	.left-created{
+		width: 67.666667%;
+	span{
+		font-size: 12px;
+	}
 	}
 </style>
