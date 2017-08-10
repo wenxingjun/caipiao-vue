724a8b90ba6383b2397ab5f2daeee8e604e2c322
wenxingjun <254317842@qq.com>
2017-08-10 16:25:11 +0800

    添加了通过token登录

--- src/js/services/global-service.js
+++ src/js/services/global-service.js
@@ -1,91 +1,85 @@
 
 export default{
 	//判断当前用户信息是否登录
-	// isLogin() {
-	// 	if(this.getLoginUserInfo().numi_auth){
-	// 		return true;
-	// 	}
-     //    return false;
-    // },
 	isLogin() {
-		if(this.getLoginUserInfo()){
+		if(this.getLoginUserInfo().token){
 			return true;
 		}
 		return false;
 	},
-    //获取启动项标志
-    getStartFlag(){
-    	//如果不是app始终返回true
-    	if(!app.Config.isApp) {
-    		return true;
-    	}
-    	const _site_local_storage = app.getSiteLocalStorage();
-    	if(_site_local_storage.startInfo) {
-    		return (_site_local_storage.startInfo.flag === true && _site_local_storage.startInfo.version === app.Config.startVersion);
-    	}
-    	return false;
-    },
+	//获取启动项标志
+	getStartFlag(){
+		//如果不是app始终返回true
+		if(!app.Config.isApp) {
+			return true;
+		}
+		const _site_local_storage = app.getSiteLocalStorage();
+		if(_site_local_storage.startInfo) {
+			return (_site_local_storage.startInfo.flag === true && _site_local_storage.startInfo.version === app.Config.startVersion);
+		}
+		return false;
+	},
 
-    //设置启动项标志
-    setStartFlag(flag){
-    	const _site_local_storage = app.getSiteLocalStorage();
+	//设置启动项标志
+	setStartFlag(flag){
+		const _site_local_storage = app.getSiteLocalStorage();
 		if(_site_local_storage.startInfo == null || typeof(_site_local_storage.startInfo) != "object"){
 			_site_local_storage.startInfo = {};
 		}
 		Object.assign(_site_local_storage.startInfo, {flag: flag, version: app.Config.startVersion});
 		app.utils.localStorage("siteLocalStorage", JSON.stringify(_site_local_storage));
-    },
+	},
 
-    //获取用户登录的Token信息
-    getLoginUserInfo(){
-    	const [_currentTime, _userInfo] = [(new Date()).getTime(), app.getSiteLocalStorage().userInfo || {}];
-    	if(_userInfo.expiredTime && (_userInfo.expiredTime - _currentTime) > 0) {
-    		return _userInfo;
-    	} else {
-    		app.globalService.setUserInfo({});
-    		return {};
-    	}
-    },
+	//获取用户登录的Token信息
+	getLoginUserInfo(){
+		const [_currentTime, _userInfo] = [(new Date()).getTime(), app.getSiteLocalStorage().userInfo || {}];
+		if(_userInfo.expiredTime && (_userInfo.expiredTime - _currentTime) > 0) {
+			return _userInfo;
+		} else {
+			app.globalService.setUserInfo({});
+			return {};
+		}
+	},
 
-    //退出登录
-    logOut(){
-    	app.globalService.setUserInfo({});
-    },
+	//退出登录
+	logOut(){
+		app.globalService.setUserInfo({});
+	},
 
-    //设置用户信息
-    setUserInfo({tenancyName, numi_auth, expiredTime = -1}) {
-    	if(expiredTime > 0) {
-    		const _site_local_storage = app.getSiteLocalStorage();
+	//设置用户信息
+	setUserInfo({tenancyName, token, expiredTime = -1}) {
+		if(expiredTime > 0) {
+			const _site_local_storage = app.getSiteLocalStorage();
 			if(_site_local_storage.userInfo == null || typeof(_site_local_storage.userInfo) != "object"){
 				_site_local_storage.userInfo = {};
 			}
 			expiredTime = (new Date()).getTime() + (expiredTime - 60) * 1000;
-			Object.assign(_site_local_storage.userInfo, {tenancyName, numi_auth, expiredTime, version: app.Config.innerVersion});
-    		app.utils.localStorage("siteLocalStorage", JSON.stringify(_site_local_storage));
-    	} else {
-    		app.utils.localStorage("siteLocalStorage", "{}");
-    	}
-    },
+			Object.assign(_site_local_storage.userInfo, {tenancyName, token, expiredTime, version: app.Config.innerVersion});
+			app.utils.localStorage("siteLocalStorage", JSON.stringify(_site_local_storage));
+		} else {
+			app.utils.localStorage("siteLocalStorage", "{}");
+		}
+	},
 
-    //app更新升级
-    updateApp(){
-    	//mui.os.plus && !mui.os.stream && mui.plusReady(update);
-    	app.ajax({
-    		url: "", //更新URL
-    		data: {
-    			"appid": plus.runtime.appid,
+	//app更新升级
+	updateApp(){
+		//mui.os.plus && !mui.os.stream && mui.plusReady(update);
+		app.ajax({
+			url: "", //更新URL
+			data: {
+				"appid": plus.runtime.appid,
 				"version": plus.runtime.version,
 				"imei": plus.device.imei
-    		},
-    		success: function(data){
-    			if (data.status) {
+			},
+			success: function(data){
+				if (data.status) {
 					plus.nativeUI.confirm(data.note, function(event) {
 						if (0 == event.index) {
 							plus.runtime.openURL(data.url);
 						}
 					}, data.title, ["立即更新", "取　　消"]);
 				}
-    		}
-    	});
-    }
+			}
+		});
+	}
 }
